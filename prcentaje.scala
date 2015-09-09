fbb424d810f6edc752f3646f139b3fd7302f3b04
alondritha <agalindo947@gmail.com>
2015-09-09 17:53:00 -0500

    tarea 1
    ejercicio 1.2

---
+++ prcentaje.scala
@@ -0,0 +1,23 @@
+
+println("cantidad 1")
+ val cantidad1 = readFloat()
+
+println("cantidad 2")
+ val cantidad2 = readFloat()
+
+println("cantidad 3")
+ val cantidad3 = readFloat()
+
+val total= cantidad1 + cantidad2 + cantidad3
+ 
+println("porcentaje 1")
+ val porcentaje1 = (cantidad1 * 100)/total
+  println ("  " + porcentaje1)
+
+println("porcentaje 2")
+ val porcentaje2 = (cantidad2 * 100)/total
+ println (" " +porcentaje2)
+
+println("porcentaje 3")
+ val porcentaje3 = (cantidad3 * 100)/total
+   println (" " +porcentaje2)
