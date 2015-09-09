512015fb256aee887b163f15ae27978f2c4d2807
alondritha <agalindo947@gmail.com>
2015-09-09 18:45:28 -0500

    tarea 1
    ejercicio 1 3 hsopital

---
+++ hospital.scala
@@ -0,0 +1,10 @@
+println("ingresar la cantidad del presupuesto disponible a repartir")
+
+  val presupuesto = readFloat()
+  val ginecologia = presupuesto * .4
+  val traumatologia = presupuesto * .3
+  val pediatria = presupuesto * .3
+
+println("el presupuesto para ginecologia es:" + ginecologia)
+println ("el presupuesto para traumatologia es:" + traumatologia)
+println("el presupuesto para pediatria es:" + pediatria)
